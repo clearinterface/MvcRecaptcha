1b81de5a30abf059633a5ef13c7eb78eaa6eff27
Eric Lee <eric@clearinterface.com>
2012-11-09 02:45:13 -0500

    Inital Commit

---
+++ TestRecaptcha/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TestRecaptcha;
+using Recaptcha;
+
+namespace TestRecaptcha.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Welcome to ASP.NET MVC!";
+
+            return View();
+        }
+
+
+        [HttpPost]
+        [RecaptchaControlMvc.CaptchaValidator]
+        public ActionResult Index(String name, bool captchaValid, String captchaErrorMessage)
+        {
+
+
+
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ TestRecaptcha/CustomHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Recaptcha;
+
+namespace MvcHelper
+{
+    public static class CustomHelper
+    {
+
+
+        //
+        public static MvcHtmlString GenerateCaptchaHere(this HtmlHelper htmlHelper)
+        {
+            RecaptchaControlMvc.PrivateKey = "Your Private Key here";
+            RecaptchaControlMvc.PublicKey = "Your Public Key";
+
+            var html = Recaptcha.RecaptchaControlMvc.GenerateCaptcha(htmlHelper);
+            return MvcHtmlString.Create(html);
+        }
+
+
+    }
+}
